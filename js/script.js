44ab6568c3561d9238af03edf58ba2734c26f809
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-04-06 21:21:18 +0000

    tweaked

--- js/script.js
+++ js/script.js
@@ -6,18 +6,15 @@
 "use strict"
 
 /**
- * This function calculates area and perimeter of rectangle.
+ * This function calculates area and perimeter of a trapezoid.
  */
 function calcAreaClicked () {
   // get user input
   let side_a = parseFloat(document.getElementById('side-a').value)
   let side_b = parseFloat(document.getElementById('side-b').value)
 	let height = parseFloat(document.getElementById('height').value)
-
-
-  // calculate the area
+  // calculate area
   let area = (side_a + side_b) / 2 * height 
-
   // display the results
   document.getElementById('area').innerHTML = "The area is " + area.toFixed(2) + "cm<sup>2</sup>"
 }
